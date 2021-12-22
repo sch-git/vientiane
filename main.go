6f3cbf59c59e320474fa59489df29476227d8561
suchenghao10349 <suchenghao10349@ipalfish.com>
2021-12-23 02:50:28 +0800

    fix: merge

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-<<<<<<< HEAD
 	"github.com/golang/glog"
 	"google.golang.org/grpc"
-=======
-	"google.golang.org/grpc"
-	"log"
->>>>>>> master
 	"net"
 	"vientiane/pub/adapter"
 	vientiane "vientiane/pub/idl/grpc"
@@ -21,19 +16,11 @@ func main() {
 
 	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", adapter.Port))
 	if nil != err {
-<<<<<<< HEAD
 		glog.Fatalln(err)
-=======
-		log.Fatalln(err)
->>>>>>> master
 	}
 
 	err = grpcServer.Serve(listen)
 	if nil != err {
-<<<<<<< HEAD
 		glog.Fatalln(err)
-=======
-		log.Fatalln(err)
->>>>>>> master
 	}
 }
